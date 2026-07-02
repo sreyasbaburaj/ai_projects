21647d23b7c3417eb90ffc6fe6fb4b8d4919086f
sreyasbaburaj <sreyasbaburaj11@gmail.com>
2026-07-02 16:54:32 +0300

    Add files via upload

---
+++ Fact_Generator.py
@@ -0,0 +1,17 @@
+import requests
+
+url="https://uselessfacts.jsph.pl/api/v2/facts/random?language=en"
+
+def get_random_fact():
+    response=requests.get(url)
+    if response.status_code==200:
+        fact_data=response.json()
+        print(f"Did you know? {fact_data['text']}")
+    else:
+        print("Failed to fetch fact")
+
+while True:
+    user_input=input("Press Enter to get a random fact or type 'q' to quit.")
+    if user_input.lower()=="q":
+        break
+    get_random_fact()
