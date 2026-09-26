aa1cd8f058b1e66f30bbc9c27af774368134291b
sreyasbaburaj <sreyasbaburaj11@gmail.com>
2026-09-26 14:19:34 +0300

    Add files via upload

--- voice_analysis_lab_2.py
+++ voice_analysis_lab_2.py
@@ -55,3 +55,59 @@ def transcribe(data, rate, width):
         return recognizer.recognize_google(AudioData(data, rate, width))
     except:
         return "[could not transcribe]"
+
+def display_stats(stats, text, label):
+    print(f"\n{'-' * 35}")
+    print(f"{label}")
+    print(f"\n{'-' * 35}")
+    print(f"Duration: {stats['duration']:.2f} sec")
+    print(f"Avg Volume: {stats['avg_volume']:.0f} ")
+    print(f"Max Volume: {stats['max_volume']:.0f}")
+    print(f"Text: {text}")
+
+def compare(s1,s2):
+    print("\n" + "=" * 40)
+    print("Comparison Results")
+    print("=" * 40)
+    longer="1" if s1["duration"] > s2["duration"] else "2"
+    print(f"Recording {longer} is longer ({s1['duration']:.1f}s vs {s2['duration']:.1f}s)")
+    louder = "1" if s1['avg_volume'] > s2['avg_volume'] else "2"
+    print(f"Recording {louder} is louder ({s1['avg_volume']:.0f}s vs {s2['avg_volume']:.0f}s)")
+    print("\n in L3, you'll CONTROL rate & volume when AI speaks!")
+
+def plot_both(s1,s2,rate):
+    fig, (ax1,ax2)=plt.subplots(2,1, figsize=(10,5))
+    t1=np.linspace(0, len(s1['samples']) / rate, len(s1['samples']))
+    ax1.plot(t1,s1['samples'], color="blue")
+    ax1.set_title("Recording 1 (Normal)")
+    ax1.set_ylabel("Amplitude")
+    ax1.grid(True, alpha=0.3)
+    t2=np.linspace(0,len(s2["samples"]) / rate, len(s2['samples']))
+    ax2.plot(t2,s2["samples"], color="red")
+    ax2.set_title("Recording 2 (Modified)")
+    ax2.set_xlabel("Time (sec)")
+    ax2.set_ylabel("Amplitude")
+    ax2.grid(True, alpha=0.3)
+    plt.tight_layout()
+    plt.show()
+
+def main():
+    print("=" * 40)
+    print("Voice Analysis Lab")
+    print("=" * 40)
+    print("record twice and compare your voice!")
+
+    audio1,rate,width=record_audio("Recording 1 :Speak NORMALLY")
+    stats1,text1=analyse_audio(audio1,rate), transcribe(audio1,rate,width)
+    display_stats(stats1, text1, "Recording 1")
+
+    input("\n Press Enter, then speak LOUDER or FASTER...")
+    audio2,rate,width=record_audio("Recording 2 :CHANGE your voice")
+    stats2,text2=analyse_audio(audio2,rate), transcribe(audio2,rate,width)
+    display_stats(stats2, text2, "Recording 2")
+
+    compare(stats1, stats2)
+    plot_both(stats1, stats2, rate)
+
+if __name__=="__main__":
+    main()
